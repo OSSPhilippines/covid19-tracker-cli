41238ee3446422fcabf1fcbbeade1f1e64d3b815
jkga <johnkennethgibasabella@gmail.com>
2020-04-17 20:17:38 +0800

    fix: api url

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ app.get('/history/charts/:country/:chartSize(sm|md|lg)?', async (req, res, next)
         chartType = req.params.chartType || 'cases',
         chartSize = req.params.chartSize || 'sm',
         summary = await axios.get(`${apiBaseURL}/countries/${countryData}`),
-        history = await axios.get(`${apiBaseURL}/v2/historical/${summary.data.country}?lastdays=all`),
+        history = await axios.get(`${apiBaseURL}/historical/${summary.data.country}?lastdays=all`),
         s = summary.data,
         h = history.data;
 
