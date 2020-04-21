994f619ef9e1197c910329b385c57f210d34a315
ianvizarra <ian.vizarra@gmail.com>
2020-04-21 23:56:58 +0800

    Fixed formatting

--- bin/util/index.js
+++ bin/util/index.js
@@ -1,14 +1,13 @@
-const { Readable } = require('stream');
+const { Readable } = require("stream");
 
-const isCommandline = (userAgent) => {
-  return (userAgent.search(/curl|wget|httpie/i) !== -1);
-};
+const isCommandline = (userAgent) =>
+  userAgent.search(/curl|wget|httpie/i) !== -1;
 
 const getStream = (req, res) => {
   const stream = new Readable();
   stream.pipe(res);
   stream._read = () => {};
-  req.on('close', () => {
+  req.on("close", () => {
     stream.destroy();
   });
   return stream;
