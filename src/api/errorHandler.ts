d1373bf1ee4aebf189b5079350d87ec4880ea139
scinorandex <scinorandex@gmail.com>
2021-03-29 16:05:32 +0800

    Add missing newline

--- src/api/errorHandler.ts
+++ src/api/errorHandler.ts
@@ -3,6 +3,6 @@ import { ErrorRequestHandler } from "express";
 export const errorHandler: ErrorRequestHandler = (error, _, res, _next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode);
-    res.send(error.message);
+    res.send(error.message + "\n");
     res.end();
 };
