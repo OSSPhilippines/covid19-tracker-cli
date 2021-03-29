0fd572d3fa2e950a16715609996ba206aa89c82f
scinorandex <scinorandex@gmail.com>
2021-03-29 16:01:03 +0800

    Remove styling from error handler for better compatibility

--- src/api/errorHandler.ts
+++ src/api/errorHandler.ts
@@ -1,9 +1,8 @@
 import { ErrorRequestHandler } from "express";
-import { generateColorTable } from "../utils/generateTable";
 
 export const errorHandler: ErrorRequestHandler = (error, _, res, _next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode);
-    res.send(generateColorTable([error.message], "red"));
+    res.send(error.message);
     res.end();
 };
