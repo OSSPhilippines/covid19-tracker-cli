b600e22af4a5c12a6c59370b968947d8ea4f7251
Jun Santilla <junsantilla@live.com>
2021-03-30 17:45:53 +0800

    Fix timezone, use standard time

--- src/utils/getTimestamp.ts
+++ src/utils/getTimestamp.ts
@@ -1,24 +1,8 @@
 /**
  *
- * @param timestamp Timestamp in Epoch Time
- * @returns String in form of As of MM/DD/YYYY, HH:mm:SS AM/PM [Date: MM/DD/YYYY]
+ * @returns String in form of standard time
  */
-export const getTimestamp: (timestamp: number) => string = (timestamp) => {
-    const date = new Date(timestamp);
-
-    const year = date.getUTCFullYear().toString();
-    const month = (date.getUTCMonth() + 1).toString();
-    const day = date.getUTCDate().toString();
-    let hour: number | string = date.getUTCHours();
-    const minute = date.getUTCMinutes().toString().padStart(2, "0");
-    const second = date.getUTCSeconds().toString().padStart(2, "0");
-
-    let ampm = hour > 11 ? "PM" : "AM";
-    hour = ampm === "PM" ? hour - 12 : hour;
-    hour = hour.toString().padStart(2, "0");
-
-    const monthDayYear = `${month}/${day}/${year}`;
-    const hourMinuteSecond = `${hour}:${minute}:${second}`;
-
-    return `As of ${monthDayYear}, ${hourMinuteSecond} ${ampm} [Date: ${monthDayYear}]`;
+export const getTimestamp = () => {
+	let dateTime = new Date()
+    return `As of ${dateTime}`;
 };
