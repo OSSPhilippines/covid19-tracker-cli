15ea08118b97952847e7148299cfa12196ef1ac8
Waren Gonzaga <github@warengonzaga.com>
2021-04-04 00:07:55 +0800

    👌 IMPROVE: Update info

--- src/utils/getResponses.ts
+++ src/utils/getResponses.ts
@@ -10,7 +10,7 @@ export const lines = {
     helpMessage: `Help: Try to append the URL with /help to learn more...`,
     sponsorMessage: `Love this project? Help us to help others by means of coffee!\n`,
     BMCLink: `(Buy Us a Coffee) wareneutron.com/donate`,
-    twitterPlug: `Follow me on twitter for more updates!\n`,
+    twitterPlug: `Follow the lead dev on twitter for more updates!\n`,
     handleHashtag: ["@warengonzaga", "#covid19trackercli"],
     docsLink: "Docs: docs.wareneutron.com/covid19-tracker-cli",
     WNrepoLink: "Repo: repo.wareneutron.com/covid19-tracker-cli",
